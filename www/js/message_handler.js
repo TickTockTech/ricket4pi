2c26cdfa690a223c7ec7bc282022fb71a2b4dda0
Allan Robertson <allan@yisup.net>
2018-05-18 21:12:55 +0100

    Add to sensor display

--- www/js/message_handler.js
+++ www/js/message_handler.js
@@ -1,4 +1,22 @@
+function sensor_data(data)
+{
+	var td_irL = document.getElementById("irL"),
+	    td_irR = document.getElementById("irL"),
+	    td_lineL = document.getElementById("irL"),
+	    td_lineR = document.getElementById("irL"),
+	    td_dist = document.getElementById("dist");
+
+	debugger
+}
+
 function handleMessage(msg, data)
 {
 	console.log("In: " + msg + ", Data: " + JSON.stringify(data));
+
+	switch(msg)
+	{
+		case MSG_SENSOR_DATA:
+			sensor_data(data);
+			break;
+	}
 }
