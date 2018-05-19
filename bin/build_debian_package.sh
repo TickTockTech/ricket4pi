dbd19b619e8fbf73f4a1c483eedcb048889891be
Allan Robertson <allan@yisup.net>
2018-05-19 12:22:28 +0100

    Fix package permissions

--- bin/build_debian_package.sh
+++ bin/build_debian_package.sh
@@ -23,6 +23,12 @@ cp $BASE_FOLDER/src/*.py debian_package/opt/$PROJECT_NAME/src/
 cp -r $BASE_FOLDER/www/*.* debian_package/opt/$PROJECT_NAME/www/
 cp $BASE_FOLDER/scripts/ricket.conf debian_package/etc/supervisor/conf.d/
 cp $BASE_FOLDER/scripts/debian_control debian_package/DEBIAN/control
+cp $BASE_FOLDER/scripts/postinst debian_package/DEBIAN/
+cp $BASE_FOLDER/scripts/postrm debian_package/DEBIAN/
+
+echo "Fixing permissions."
+chmod 755 debian_package/DEBIAN/postinst
+chmod 755 debian_package/DEBIAN/postrm
 
 echo "Building package."
 dpkg-deb --build debian_package
@@ -34,7 +40,7 @@ echo "Changing back to directory: $ORIGINAL_PATH."
 cd $ORIGINAL_PATH
 
 echo "Tidying up"
-#rm -rf $BASE_FOLDER/bin/debian_package
+rm -rf $BASE_FOLDER/bin/debian_package
 
 echo ""
 echo "Package name: ${PACKAGE_NAME}_${PACKAGE_VERSION}_all.deb"
