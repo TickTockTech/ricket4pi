7a6f6d87f21c7386803114021f466b69f06006ef
Allan_mac <allan@yisup.net>
2018-05-19 08:33:02 +0000

    Fix debian package build

--- bin/build_debian_package.sh
+++ bin/build_debian_package.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-package_name=ricket4pi
-package_version="0.0.1-1"
+PACKAGE_NAME=ricket4pi
+PACKAGE_VERSION="0.0.1-1"
 
-original_path=$(pwd)
-script_name=$0
-script_path=$(dirname "$0")
-base_folder="$( cd "$(dirname "$0")/../" ; pwd -P )"
-project_folder=ricket4pi
+ORIGINAL_PATH=$(pwd -P)
+SCRIPT_NAME=$0
+SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+BASE_FOLDER="$( cd "$(dirname "$0")/../" ; pwd -P )"
+PROJECT_NAME=ricket4pi
 
-echo "Changing to directory: $(script_path)."
-cd $(script_path)
+echo "Changing to directory: $SCRIPT_PATH."
+cd $SCRIPT_PATH
 
 echo "Creating folder structure."
-mkdir -p debian_package/opt/$(project_folder)
+mkdir -p debian_package/opt/$PROJECT_NAME/www
+mkdir -p debian_package/opt/$PROJECT_NAME/src
 mkdir -p debian_package/usr/local/share/supervisor/conf.d/
+mkdir -p debian_package/DEBIAN/
 
 echo "Creating scripts."
-cp $(base_folder)/src/*.py debian_package/opt/$(project_folder)/src/
-cp -r $(base_folder)/www/*.* debian_package/opt/$(project_folder)/www/
-cp ricket.conf debian_package/usr/local/share/supervisor/conf.d/
+cp $BASE_FOLDER/src/*.py debian_package/opt/$PROJECT_NAME/src/
+cp -r $BASE_FOLDER/www/*.* debian_package/opt/$PROJECT_NAME/www/
+cp $BASE_FOLDER/scripts/ricket.conf debian_package/usr/local/share/supervisor/conf.d/
+cp $BASE_FOLDER/scripts/debian_control debian_package/DEBIAN/control
 
 echo "Building package."
 dpkg-deb --build debian_package
 
-mv debian_package.deb $(package_name)_$(package_version)_all.deb
+echo "Renaming to ${PACKAGE_NAME}_${PACKAGE_VERSION}_all.deb"
+mv debian_package.deb ${PACKAGE_NAME}_${PACKAGE_VERSION}_all.deb
 
-echo "Changing back to directory: $(original_path)."
-cd $(original_path)
+echo "Changing back to directory: $ORIGINAL_PATH."
+cd $ORIGINAL_PATH
+
+echo "Tidying up"
+rm -rf $BASE_FOLDER/bin/debian_package
 
 echo ""
-echo "Package name: $(package_name)_$(package_version)_all.deb"
-echo "Package available in: $(script_path)"
+echo "Package name: ${PACKAGE_NAME}_${PACKAGE_VERSION}_all.deb"
+echo "Package available in: $SCRIPT_PATH"
+
